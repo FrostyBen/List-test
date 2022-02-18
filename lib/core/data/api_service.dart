ac1cc737c2f71fd569f33d5f444936b544b7e203
FrostyBen <frostiben@gmail.com>
2022-02-18 21:05:38 +0300

    data fixes

---
+++ lib/core/data/api_service.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+
+import 'package:task_flutter_test/core/bloc/bloc/chapter_bloc.dart';
+import 'package:task_flutter_test/core/data/models/result_model.dart';
+
+class ApiService {
+  String jsonString = '''[
+    {
+        "title": "Теория",
+        "chapters": [
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            }
+        ]
+    },
+    {
+        "title": "Практика",
+        "chapters": [
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            }
+        ]
+    },
+    {
+        "title": "Тесты",
+        "chapters": [
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            },
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            }
+        ]
+    },
+    {
+        "title": "Сертификат",
+        "chapters": [
+            {
+                "dateTime": "2022...",
+                "text": "главы..."
+            }
+        ]
+    }
+]''';
+  Future<ChaptersData> getChapters() async {
+    return ChaptersData.fromJson(
+      jsonDecode(jsonString),
+    );
+  }
+}
